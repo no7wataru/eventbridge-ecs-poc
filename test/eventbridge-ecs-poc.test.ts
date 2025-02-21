04d5b4d1d8bb9dba1ac6e38e363d70e10dc2f2aa
Wataru Yoshioka <no7.wataru@gmail.com>
2025-02-21 18:37:08 +0900

    Initial commit

---
+++ test/eventbridge-ecs-poc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EventbridgeEcsPoc from '../lib/eventbridge-ecs-poc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eventbridge-ecs-poc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EventbridgeEcsPoc.EventbridgeEcsPocStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
