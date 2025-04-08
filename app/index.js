a5acbbeb480e9496fd8f4ad7bbd8f186a407cb41
Wataru Yoshioka <no7.wataru@gmail.com>
2025-04-08 17:54:59 +0900

    Small fix

--- app/index.js
+++ app/index.js
@@ -1,1 +1,2 @@
-console.log("Task type:", process.env.TASK_TYPE); console.log("Received S3 File Path:", process.env.S3_FILE_PATH);
+console.log("Task type:", process.env.TASK_TYPE);
+console.log("Received S3 File Path:", process.env.S3_FILE_PATH);
